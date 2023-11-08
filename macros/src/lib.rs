ba4739bf1f72411308714f9afd211bfd1e38859d
Adam Curtis <kallisti.dev@gmail.com>
2023-11-08 10:13:36 -0500

    clean up test attr normalization

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,9 +1,13 @@
 mod test;
-use syn::ItemFn;
+use syn::{ItemFn, Meta};
 use test::gen_test;
 
 #[proc_macro_attribute]
-pub fn test(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let test_fn = syn::parse::<ItemFn>(item).expect("Could not parse integration_test function");
-    gen_test(test_fn).into()
+pub fn test(
+    attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let attr_meta = syn::parse::<Meta>(attr).expect("Could not parse test attribute");
+    let test_fn = syn::parse::<ItemFn>(item).expect("Could not parse test function");
+    gen_test(attr_meta, test_fn).into()
 }
--- macros/src/test.rs
+++ macros/src/test.rs
@@ -1,25 +1,16 @@
+use proc_macro2::Span;
 use quote::quote;
-use syn::ItemFn;
+use syn::{parse_quote_spanned, spanned::Spanned, token::Bracket, Attribute, ItemFn, Meta, Token};
 
-pub(crate) fn gen_test(test_fn: ItemFn) -> proc_macro2::TokenStream {
+pub(crate) fn gen_test(attr_meta: Meta, test_fn: ItemFn) -> proc_macro2::TokenStream {
+    // add #[test] if missing
     let ItemFn {
         attrs,
         vis,
         sig,
         block,
-    } = test_fn;
-    // add #[test] if missing
-    let test_attr = if attrs
-        .iter()
-        .find(|&attr| attr.path().is_ident("test"))
-        .is_none()
-    {
-        Some(quote! {#[test]})
-    } else {
-        None
-    };
+    } = normalize_fn_test_attr(attr_meta.span(), test_fn);
     quote! {
-        #test_attr
         #(#attrs)*
         #vis #sig
         {
@@ -31,5 +22,34 @@ pub(crate) fn gen_test(test_fn: ItemFn) -> proc_macro2::TokenStream {
             std::panic::set_hook(_orig_hook);
             ret
         }
+    }
+}
+
+/// Modifies a function definition to include a `#[test]` attribute macro if none is currently
+/// provided.
+pub(crate) fn normalize_fn_test_attr(span: Span, mut item_fn: ItemFn) -> ItemFn {
+    if fn_has_test_attr(&item_fn) {
+        item_fn
+    } else {
+        item_fn.attrs.push(gen_test_attr(span));
+        item_fn
+    }
+}
+
+/// check if an ItemFn has a `#[test]` attribute macro
+pub(crate) fn fn_has_test_attr(ItemFn { attrs, .. }: &ItemFn) -> bool {
+    attrs
+        .iter()
+        .find(|&attr| attr.path().is_ident("test"))
+        .is_none()
+}
+
+/// Generate a `#[test]` attribute in the given Span
+pub(crate) fn gen_test_attr(span: Span) -> Attribute {
+    Attribute {
+        pound_token: Token![#]([span]),
+        style: syn::AttrStyle::Outer,
+        bracket_token: Bracket(span),
+        meta: parse_quote_spanned!(span => test),
     }
 }
